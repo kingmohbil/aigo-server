dc2ea2254b44c6bdf7cef3c44b1751a690945b56
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-04 22:54:54 +0300

    Added support for 6 languages

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-//* Routers
-const googleRouter = require('./routes/google');
-const amazonRouter = require('./routes/amazon');
-const deepgramRouter = require('./routes/deepgram');
-
 const { speech_to_text } = require('./lib/deepgram/speech_api');
-const { text_to_speech } = require('./lib/google/speech_api');
+const {
+  text_to_speech,
+  speech_to_text: GoogleSpeech,
+} = require('./lib/google/speech_api');
 const { chat } = require('./lib/openai/chat');
 
-const PORT = process.env.port || 3000;
+const PORT = 3000;
 
 const io = new Server(server, {
   cors: {
@@ -29,23 +27,38 @@ ws.on('connection', (socket) => {
   console.log(`Socket with id: ${socket.id} connected`);
   ws.to(socket.id).emit('play-question', 'play question started');
 
-  socket.on('stream-voice', async (data) => {
-    const question = await speech_to_text(data);
-    console.log(`Question: ${question}`);
-    const answer = await chat(question);
-    console.log(`Answer: ${answer}`);
-    const speech = await text_to_speech(answer);
-    ws.to(socket.id).emit('text', speech, answer);
-  });
+  socket.on(
+    'stream-voice',
+    async (data, lang = { speech: 'en-US', text: 'en-US' }) => {
+      let question = '';
+      try {
+        if (lang.speech === 'ar-XA')
+          question = await GoogleSpeech(data, lang.speech);
+        else question = await speech_to_text(data, lang.speech);
+
+        console.log(`Question: ${question}`);
+
+        const answer = await chat(question);
+        console.log(`Answer: ${answer}`);
+
+        const speech = await text_to_speech(answer, lang.text);
+        ws.to(socket.id).emit('test-text', speech, answer);
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+    }
+  );
 
   socket.conn.on('close', (reason) => {
-    console.log(`Socket with id: ${socket.id} disconnected`);
+    console.log(
+      `Socket with id: ${socket.id} disconnected with reason: ${reason}`
+    );
   });
 });
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-server.listen(PORT, '192.168.0.104', () => {
+server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
