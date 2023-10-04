0fbaef75b9dc973783258390a76d25a91ee960d5
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-04 23:04:23 +0300

    Changed the websocket event name

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ ws.on('connection', (socket) => {
         console.log(`Answer: ${answer}`);
 
         const speech = await text_to_speech(answer, lang.text);
-        ws.to(socket.id).emit('test-text', speech, answer);
+        ws.to(socket.id).emit('text', speech, answer);
       } catch (error) {
         console.log(`Error: ${error}`);
       }
